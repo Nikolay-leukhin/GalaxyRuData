4cdb5c6ea428a78e9e2567d77caf136f5cbed764
domager73 <112970685+domager73@users.noreply.github.com>
2023-11-08 21:33:29 +0300

    check ui

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/cupertino.dart';
-import 'package:galaxy_rudata/feature/auth/ui/pages/login_screen.dart';
 import 'package:galaxy_rudata/feature/auth/ui/pages/pin_screen.dart';
 import 'package:galaxy_rudata/feature/access_code/ui/lock_screen.dart';
-import 'package:galaxy_rudata/feature/auth/ui/login_screen.dart';
 import 'package:galaxy_rudata/feature/certificate/ui/nft_certificate.dart';
 import 'package:galaxy_rudata/feature/safe/ui/safe_screen.dart';
 import 'package:galaxy_rudata/routes/route_names.dart';
 
 Map<String, Widget Function(BuildContext)> appRoutes = {
-  // RouteNames.authEnterEmail: (context) => const LoginScreen(),
   RouteNames.root: (context) => const LockScreen(),
   RouteNames.nftCertificate: (context) => const NftCertificateScreen(),
   RouteNames.accessCodeLock: (context) => const LockScreen(),
