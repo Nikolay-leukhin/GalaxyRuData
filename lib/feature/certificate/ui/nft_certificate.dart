8dc8e32fb585628bd3bb20497a4c64404a197b6b
domager73 <112970685+domager73@users.noreply.github.com>
2023-11-08 17:48:25 +0300

    ADDED: - full certificate screen

--- lib/feature/certificate/ui/nft_certificate.dart
+++ lib/feature/certificate/ui/nft_certificate.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:galaxy_rudata/utils/utils.dart';
 import 'package:galaxy_rudata/widgets/app_bar_items/actions_container.dart';
 import 'package:galaxy_rudata/widgets/app_bar_items/rf_container.dart';
+import 'package:galaxy_rudata/widgets/buttons/custom_button.dart';
 
 class NftCertificateScreen extends StatefulWidget {
   const NftCertificateScreen({super.key});
@@ -13,6 +15,8 @@ class NftCertificateScreen extends StatefulWidget {
 class _NftCertificateScreenState extends State<NftCertificateScreen> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+
     return Container(
       decoration: const BoxDecoration(
           image: DecorationImage(
@@ -29,8 +33,61 @@ class _NftCertificateScreenState extends State<NftCertificateScreen> {
             children: [ActionsContainer(), RfContainer()],
           ),
         ),
-        body: const Column(
-          children: [],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Поздравляем! Вы получили NFT-сертификат собственного жилья во Вселенной Большого Росреестра!',
+                    style: AppTypography.font16w400,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    width: size.width - 64,
+                    height: (size.width - 64) * 0.854,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26.withOpacity(0.5),
+                            spreadRadius: 0.5,
+                            blurRadius: 10,
+                            offset: const Offset(0, 0),
+                          ),
+                        ],
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/certificate.png'),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  CustomButton(
+                      content: Text(
+                        'Посмотреть в кошельке'.toUpperCase(),
+                        style: AppTypography.font16w600,
+                      ),
+                      onTap: () {},
+                      width: double.infinity),
+                  const SizedBox(height: 20,),
+                  CustomButton(
+                      content: Text(
+                        'Забронировать ещё жилье!'.toUpperCase(),
+                        style: AppTypography.font16w600,
+                      ),
+                      onTap: () {},
+                      width: double.infinity),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
