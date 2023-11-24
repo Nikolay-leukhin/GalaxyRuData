d0074f3092f87b6dc4b4b811588169f0d11fe3d9
Jason1258t <ivandomogatskij@gmail.com>
2023-11-24 20:12:15 +0300

    3 правка но сомневаюсь что работает

--- lib/feature/auth/ui/pages/login_screen.dart
+++ lib/feature/auth/ui/pages/login_screen.dart
@@ -8,6 +8,7 @@ import 'package:galaxy_rudata/utils/validators.dart';
 import 'package:galaxy_rudata/widgets/app_bars/main_app_bar.dart';
 import 'package:galaxy_rudata/widgets/buttons/custom_button.dart';
 import 'package:galaxy_rudata/widgets/popup/custom_popup.dart';
+import 'package:galaxy_rudata/widgets/scaffolds/main_scaffold.dart';
 import 'package:galaxy_rudata/widgets/text_fields/base_text_form_field.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -95,8 +96,6 @@ class _LoginScreenState extends State<LoginScreen> {
               ));
         } else if (state is AuthSuccessState) {
           Dialogs.hide(context);
-
-          // Navigator.pushNamed(context, RouteNames.walletCard);
         } else if (state is AuthLoadingState) {
           Dialogs.showModal(
               context,
@@ -109,182 +108,173 @@ class _LoginScreenState extends State<LoginScreen> {
         onTap: () {
           FocusScope.of(context).unfocus();
         },
-        child: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  filterQuality: FilterQuality.low,
-                  image: AssetImage("assets/images/bg.png"))),
-          child: SafeArea(
-            child: Scaffold(
-              backgroundColor: Colors.transparent,
-              resizeToAvoidBottomInset: false,
-              appBar: MainAppBar.onlyLogo(
-                context,
-              ),
-              body: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.end,
+        child: MainScaffold(
+          canPop: false,
+          // bottomResize: true,
+          appBar: MainAppBar.onlyLogo(
+            context,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "Авторизация",
+                  style: AppTypography.font24w700
+                      .copyWith(color: Colors.white),
+                ),
+                Container(
+                  height: size.height * 0.059,
+                  constraints: const BoxConstraints(maxHeight: 48),
+                ),
+                BaseTextFormField(
+                  withError: errorEmailField,
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  hintText: "E-mail адрес",
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                SizedBox(
+                  height: 60,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        child: BaseTextFormField(
+                          withError: errorCodeField,
+                          padding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 22)
+                              .copyWith(right: 120),
+                          controller: codeController,
+                          keyboardType: TextInputType.number,
+                          hintText: "Код",
+                        ),
+                      ),
+                      currentRemainingTime == 0
+                          ? Positioned(
+                              right: 0,
+                              child: CustomButton(
+                                  content: Text("Отправить".toUpperCase(),
+                                      style: AppTypography.font16w600
+                                          .copyWith(color: Colors.white)),
+                                  onTap: () {
+                                    sendAuthCode();
+                                  },
+                                  width: 120),
+                            )
+                          : Positioned(
+                              right: 0,
+                              child: CustomButton(
+                                  content: Text(
+                                      "${currentRemainingTime} СЕКУНД"
+                                          .toUpperCase(),
+                                      style: AppTypography.font16w600
+                                          .copyWith(color: Colors.white)),
+                                  onTap: () {
+                                    sendAuthCode();
+                                  },
+                                  width: 120),
+                            )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Text(
-                      "Авторизация",
-                      style: AppTypography.font24w700
-                          .copyWith(color: Colors.white),
-                    ),
                     Container(
-                      height: size.height * 0.059,
-                      constraints: const BoxConstraints(maxHeight: 48),
-                    ),
-                    BaseTextFormField(
-                      withError: errorEmailField,
-                      controller: emailController,
-                      keyboardType: TextInputType.emailAddress,
-                      hintText: "E-mail адрес",
-                    ),
-                    const SizedBox(
+                      width: 32,
                       height: 32,
-                    ),
-                    SizedBox(
-                      height: 60,
-                      child: Stack(
-                        children: [
-                          Positioned(
-                            child: BaseTextFormField(
-                              withError: errorCodeField,
-                              padding: const EdgeInsets.symmetric(
-                                      horizontal: 16, vertical: 22)
-                                  .copyWith(right: 120),
-                              controller: codeController,
-                              keyboardType: TextInputType.number,
-                              hintText: "Код",
-                            ),
-                          ),
-                          currentRemainingTime == 0
-                              ? Positioned(
-                                  right: 0,
-                                  child: CustomButton(
-                                      content: Text("Отправить".toUpperCase(),
-                                          style: AppTypography.font16w600
-                                              .copyWith(color: Colors.white)),
-                                      onTap: () {
-                                        sendAuthCode();
-                                      },
-                                      width: 120),
-                                )
-                              : Positioned(
-                                  right: 0,
-                                  child: CustomButton(
-                                      content: Text(
-                                          "${currentRemainingTime} СЕКУНД"
-                                              .toUpperCase(),
-                                          style: AppTypography.font16w600
-                                              .copyWith(color: Colors.white)),
-                                      onTap: () {
-                                        sendAuthCode();
-                                      },
-                                      width: 120),
-                                )
-                        ],
+                      decoration: BoxDecoration(
+                        color: AppColors.primary,
+                        border: Border.all(
+                            width: 2,
+                            color: !withErrorCheckBox
+                                ? AppColors.silver
+                                : Colors.red),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Transform.scale(
+                        scale: 25 / Checkbox.width,
+                        child: Checkbox(
+                          value: isConditionsAccepted,
+                          onChanged: (v) {
+                            changeCheckboxValue(v ?? false);
+                          },
+                          splashRadius: 0,
+                          fillColor:
+                              MaterialStateProperty.all(AppColors.primary),
+                          side: const BorderSide(
+                              width: 0, color: Colors.transparent),
+                          materialTapTargetSize:
+                              MaterialTapTargetSize.shrinkWrap,
+                        ),
                       ),
                     ),
                     const SizedBox(
-                      height: 32,
+                      width: 8,
                     ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          width: 32,
-                          height: 32,
-                          decoration: BoxDecoration(
-                            color: AppColors.primary,
-                            border: Border.all(
-                                width: 2,
-                                color: !withErrorCheckBox
-                                    ? AppColors.silver
-                                    : Colors.red),
-                            borderRadius: BorderRadius.circular(4),
-                          ),
-                          child: Transform.scale(
-                            scale: 25 / Checkbox.width,
-                            child: Checkbox(
-                              value: isConditionsAccepted,
-                              onChanged: (v) {
-                                changeCheckboxValue(v ?? false);
-                              },
-                              splashRadius: 0,
-                              fillColor:
-                                  MaterialStateProperty.all(AppColors.primary),
-                              side: const BorderSide(
-                                  width: 0, color: Colors.transparent),
-                              materialTapTargetSize:
-                                  MaterialTapTargetSize.shrinkWrap,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        Flexible(
-                          child: RichText(
-                              text: TextSpan(children: [
-                            TextSpan(
-                                text: "Я согласен с ",
-                                style: AppTypography.font12w400),
-                            TextSpan(
-                                text: "Пользовательским Соглашением",
-                                style: AppTypography.font12w400.copyWith(
-                                    decoration: TextDecoration.underline),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () async {
-                                    await launchUrl(Uri.parse(
-                                        'https://docs.google.com/document/d/15zGuCD50uoIJdmAC9_T8tpzzG9NDN26wRNye2Spy160/edit?usp=share_link'));
-                                  }),
-                            TextSpan(
-                                text: " и ",
-                                style: AppTypography.font12w400.copyWith(
-                                    decoration: TextDecoration.underline)),
-                            TextSpan(
-                                text: "Политикой Конфиденциальности",
-                                style: AppTypography.font12w400.copyWith(
-                                    decoration: TextDecoration.underline),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () async {
-                                    await launchUrl(Uri.parse(
-                                        'https://docs.google.com/document/d/1c6zaEfcPYEUsjOpBVnVyzt2Gb3ryIZVBX3O0id4JKik/edit?usp=share_link'));
-                                  }),
-                          ])),
-                        ),
-                      ],
+                    Flexible(
+                      child: RichText(
+                          text: TextSpan(children: [
+                        TextSpan(
+                            text: "Я согласен с ",
+                            style: AppTypography.font12w400),
+                        TextSpan(
+                            text: "Пользовательским Соглашением",
+                            style: AppTypography.font12w400.copyWith(
+                                decoration: TextDecoration.underline),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () async {
+                                await launchUrl(Uri.parse(
+                                    'https://docs.google.com/document/d/15zGuCD50uoIJdmAC9_T8tpzzG9NDN26wRNye2Spy160/edit?usp=share_link'));
+                              }),
+                        TextSpan(
+                            text: " и ",
+                            style: AppTypography.font12w400.copyWith(
+                                decoration: TextDecoration.underline)),
+                        TextSpan(
+                            text: "Политикой Конфиденциальности",
+                            style: AppTypography.font12w400.copyWith(
+                                decoration: TextDecoration.underline),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () async {
+                                await launchUrl(Uri.parse(
+                                    'https://docs.google.com/document/d/1c6zaEfcPYEUsjOpBVnVyzt2Gb3ryIZVBX3O0id4JKik/edit?usp=share_link'));
+                              }),
+                      ])),
                     ),
-                    const SizedBox(
-                      height: 32,
-                    ),
-                    CustomButton(
-                        content: Text(
-                          "Войти / Регистрация".toUpperCase(),
-                          style: AppTypography.font16w600
-                              .copyWith(color: Colors.white),
-                        ),
-                        onTap: () {
-                          final permission = checkFieldsState();
-
-                          if (permission) {
-                            context.read<AuthCubit>().auth(
-                                email: emailController.text.trim(),
-                                password: codeController.text.trim());
-                          }
-                        },
-                        width: double.infinity),
-                    SizedBox(
-                      height: size.height * 0.2,
-                    )
                   ],
                 ),
-              ),
+                const SizedBox(
+                  height: 32,
+                ),
+                CustomButton(
+                    content: Text(
+                      "Войти / Регистрация".toUpperCase(),
+                      style: AppTypography.font16w600
+                          .copyWith(color: Colors.white),
+                    ),
+                    onTap: () {
+                      final permission = checkFieldsState();
+
+                      if (permission) {
+                        context.read<AuthCubit>().auth(
+                            email: emailController.text.trim(),
+                            password: codeController.text.trim());
+                      }
+                    },
+                    width: double.infinity),
+                SizedBox(
+                  height: size.height * 0.2,
+                )
+              ],
             ),
           ),
         ),
