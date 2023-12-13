40614489cce1fa9c42e54a8425b6261e37b69112
Jason1258t <ivandomogatskij@gmail.com>
2023-12-13 09:02:09 +0300

    попытки сделать уведы

---
+++ android/app/src/main/kotlin/com.kadastr.rosreestr/MainActivity.kt
@@ -0,0 +1,11 @@
+//package com.rosreestr.kadastr
+package com.kadastr.rosreestr
+
+import io.flutter.embedding.android.FlutterActivity
+
+
+class MainActivity: FlutterActivity() {
+    init {
+        System.loadLibrary("TrustWalletCore")
+    }
+}
